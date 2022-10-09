7355c1a9f28d3d8310ea8753db5adf9106a76627
Pedro Tesch <pedrotesch.pt@gmail.com>
2022-10-09 20:27:50 -0300

    JESUS...

---
+++ fechamento/index.js
@@ -0,0 +1,221 @@
+function getRadioValue(name){
+    let valor = 'default'
+    let elements = document.getElementsByName(name)
+    for(let i = 0; i<elements.length; ++i){
+        if(elements[i].checked){
+            valor = elements[i].value
+        }
+    }
+    return valor;
+}
+
+function precoMetroQuadrado() {
+    let tipoDeMed = getRadioValue('tipoDeMed');
+    let cor = getRadioValue('cor');
+    let val1 = {
+        'color':{
+            'padrao':179.2, 
+            'eng':210,
+            'box':176.4,
+            'pia':120.00
+        },
+        'inc':{
+            'padrao':133.5,
+            'eng':154,
+            'box':123,
+            'pia':120.00
+        },
+        'default':{}    
+    } 
+    return val1[cor][tipoDeMed]
+}
+
+function medidaVidro() {
+    let aux1 = document.getElementById('altura')
+    let alt = Number(aux1.value)
+    let serv = document.getElementById('tipoDeServ').value
+    if(serv == 'trasVao' || serv == 'trasVaoD' || serv == 'trasVaoE'){ //transpasso na altura da porta de correr por trás do vão
+        alt = (alt+5)*0.01
+    }else {
+        alt *= 0.01
+    }
+    let larg = transp()
+    let p = precoMetroQuadrado()
+    precoVidro = alt * larg * p
+    console.log(`preço do vidro: ${precoVidro.toFixed(2)}`)
+    return precoVidro
+}
+
+function transp(){
+    let larg = document.getElementById('largura')
+    larg = Number(larg.value)
+    larg *= 0.01 //transformando de metro pra centímetros
+    let serv = document.getElementById('tipoDeServ').value
+    if(serv == 'j2f' ||serv == 'p2f' || serv == 'boxF' || serv == 'kitPia' || serv == 'trasVao' || serv == 'trasVaoE'){
+        larg += 0.05
+    }else if(serv == 'j4f' || serv == 'p4f' || serv == 'boxC' || serv == 'trasVaoD'|| serv == 'versJ3' || serv == 'versP3'){
+        larg += 0.1
+    }else if(serv == 'versJ6' || serv == 'versP6'){
+        larg += 0.2
+    }
+    return larg
+}
+
+function alum() { //calculo do preço dos alumínios
+    let serv = document.getElementById('tipoDeServ').value
+    let aux1 = document.getElementById('altura')
+    let alt = Number(aux1.value)
+    let corAlum = document.getElementsByName('corA')
+
+    if(serv == 12 || serv == 13){ //transpasso na altura da porta de correr por trás do vão
+        alt = (alt+5) * 0.01
+    }else {
+        alt = alt * 0.01
+    }
+    let larg = transp()
+    let precoAlum, aux, alumAlt, alumLarg
+
+        if(serv == 'j2f'){
+            alumAlt = alt * 15
+            alumLarg = larg * 39.5
+            aux = alumAlt + alumLarg
+            kit = 18
+            precoAlum = aux + kit
+        }else if(serv == 'j4f'){
+            alumAlt = alt * 24
+            alumLarg = larg * 39.5
+            aux = alumAlt + alumLarg
+            kit = 25
+            precoAlum = aux + kit
+        }else if(serv == 'p2f'){
+            alumAlt = alt * 15
+            alumLarg = larg * 39.5
+            aux = alumAlt + alumLarg
+            kit = 49
+            precoAlum = aux + kit
+        }else if(serv == 'p4f'){
+            alumAlt = alt * 24
+            alumLarg = larg * 39.5
+            aux = alumAlt + alumLarg
+            kit = 68
+            precoAlum = aux + kit
+        }else if(serv == 'boxF' ){
+            alumAlt = alt * 13
+            alumLarg = larg * 48
+            kit = 8
+            precoAlum = kit + alumAlt + alumLarg;
+        }else if(serv == 'boxC'){
+            alumAlt = alt * 15
+            alumLarg = larg * 48
+            aux = alumAlt + alumLarg
+            kit = 17
+            precoAlum = aux + kit
+        }else if(serv == 'piv'){
+            kit = 0
+            precoAlum = 115
+        }else if(serv == 'bascP'){
+            kit = 0
+            precoAlum = 40
+        }else if(serv == 'bascG'){
+            aux = 'Báscula g'
+            kit = 0
+            precoAlum = 60
+        }else if(serv == 'portaoP'){
+            precoAlum = 40
+            kit = 0
+        }else if(serv == 'portaoG'){
+            precoAlum = 70
+            kit = 0
+        }else if(serv == 'kitPia'){
+            alumAlt = alt * 13
+            alumLarg = larg * 45
+            aux = alumAlt + alumLarg
+            kit = 20
+            precoAlum = aux + kit
+        }else if(serv == 'trasVao'){
+            alumAlt = alt * 15
+            alumLarg = larg * 52 * 2
+            aux = alumAlt + alumLarg
+            kit = 60
+            precoAlum = aux + kit
+        }else if(serv == 'trasVaoE'){
+            alumAlt = alt * 15
+            alumLarg = larg * 52 * 2
+            aux = alumAlt + alumLarg
+            kit = 60
+            precoAlum = aux + kit +80//estruturação
+        }else if(serv == 'trasVaoD'){
+            alumAlt = alt * 25
+            alumLarg = larg * 52 * 2
+            aux = alumAlt + alumLarg
+            kit = 80
+            precoAlum = aux + kit
+        }else if(serv == 'pivD'){
+            precoAlum = 255
+            kit = 0
+        }else if(serv == 'versJ3'){
+            alumAlt = alt * 25
+            alumLarg = larg * 84
+            aux = alumAlt + alumLarg
+            kit = 70
+            precoAlum = aux + kit
+        }else if(serv == 'versP3'){
+            alumAlt = alt * 25
+            alumLarg = larg * 84
+            kit = 110
+            precoAlum = alumAlt + alumLarg + kit
+        }else if(serv == 'versJ6'){
+            alumAlt = alt * 25
+            alumLarg = larg * 84
+            aux = alumAlt + alumLarg
+            kit = 125
+            precoAlum = aux + kit
+        }else if(serv == 'versP6'){
+            alumAlt = alt * 25
+            alumLarg = larg * 84
+            aux = alumAlt + alumLarg
+            kit = 180
+            precoAlum = aux + kit
+        }
+
+    if(corAlum[1].checked){
+        precoAlum *= 1.05
+    }
+
+    console.log(`aluminio: ${precoAlum} (neste valor já está incluido o kit!)`)
+
+    return precoAlum
+}
+
+function vinil() {
+    let aux1 = document.getElementById('altVin')
+    let alt = Number(aux1.value)
+    let aux2 = document.getElementById('largVin')
+    let larg = Number(aux2.value)
+    alt *= 0.01
+    larg *= 0.01
+    precovin = alt *larg * 19;
+    console.log(`preço do vinil: ${precovin}`)
+    return precovin
+}
+
+function puxador() {
+    let pux = document.getElementsByName('pux')
+    let puxAux
+    if(pux[0].checked){
+        puxAux = 0
+    }else if(pux[1].checked){
+        puxAux = 26
+    }else if(pux[2].checked){
+        puxAux = 52
+    }
+    console.log(`preço do puxador: ${puxAux}`)
+    return puxAux
+}
+
+function somaFinal() {
+    let precoFinal = medidaVidro() + alum() + vinil() + puxador()
+    let teste = document.getElementById('teste')
+    teste.innerHTML = `<br>O preço de custo foi: R$${precoFinal.toFixed(2)}`.replace(".", ",")
+    console.log('FIM-------------------------') 
+}
---
+++ fechamento/metroLinear/metro.js
@@ -0,0 +1,538 @@
+function perfil1(){
+    let perfil = document.getElementById('tipoPerfil1').value;
+    let aux1 = document.getElementById('metro1');
+    let metro = Number(aux1.value);
+    let total = 0;
+
+    if(perfil == 'tuboPan'){
+        total = metro * 42;
+    }else if(perfil == 'tubo1x1'){
+        total = metro * 9;
+    }else if(perfil == 'tubo15x15'){
+        total = metro * 13;
+    }else if(perfil == 'tubo2x15'){
+        total = metro * 22;
+    }else if(perfil == 'tubo2x2'){
+        total = metro * 19;
+    }else if(perfil == 'tubo3x15'){
+        total = metro * 27;
+    }else if(perfil == 'tubo4x2'){
+        total = metro * 34;
+    }else if(perfil == 'tubo4x4'){
+        total = metro * 77;
+    }else if(perfil == 'tuboRed2'){
+        total = metro * 17;
+    }else if(perfil == 'tuboRed3'){
+        total = metro * 27;
+    }else if(perfil == 'reg5x5'){
+        total = metro * 5;
+    }else if(perfil == 'reg1x5'){
+        total = metro * 8;
+    }else if(perfil == 'reg2x5'){
+        total = metro * 15;
+    }else if(perfil == 'corre28'){
+        total = metro * 22;
+    }else if(perfil == 'cant12'){
+        total = metro * 5;
+    }else if(perfil == 'cant58'){
+        total = metro * 6;
+    }else if(perfil == 'cant34'){
+        total = metro * 7;
+    }else if(perfil == 'cant1'){
+        total = metro * 8;
+    }else if(perfil == 'jotinha'){
+        total = metro * 8;
+    }else if(perfil == 'jotao'){
+        total = metro * 14.5;
+    }else if(perfil == 'caval'){
+        total = metro * 10;
+    }else if(perfil == 'superCaval'){
+        total = metro * 16;
+    }else if(perfil == 'cantBag'){
+        total = metro * 14;
+    }else if(perfil == 'dobBox'){
+        total = metro * 21;
+    }else if(perfil == 'dobPort'){
+        total = metro * 8;
+    }else if(perfil == 'u8'){
+        total = metro * 5;
+    }else if(perfil == 'u10'){
+        total = metro * 6;
+    }else if(perfil == 'uGuard'){
+        total = metro * 11;
+    }else if(perfil == 'parTorreInox'){
+        total = metro * 0.4;
+    }else if(perfil == 'parInoxTorreInox'){
+        total = metro * 2;
+    }else if(perfil == 'torreInox'){
+        total = metro * 70;
+    }else if(perfil == 'pont15x15'){
+        total = metro * 25;
+    }else if(perfil == 'pont2x15'){
+        total = metro * 30;
+    }else if(perfil == 'ibere'){
+        total = metro * 15;
+    }else if(perfil == 'guarn39'){
+        total = metro * 1.7;
+    }else if(perfil == 'guarn44'){
+        total = metro * 0;
+    }else if(perfil == 'guarn51'){
+        total = metro * 2.4;
+    }else if(perfil == 'puxHquad'){
+        total = metro * 0;
+    }else if(perfil == 'puxHquadCrom'){
+        total = metro * 0;
+    }else if(perfil == 'puxHred'){
+        total = metro * 0;
+    }else if(perfil == 'puxHredCrom'){
+        total = metro * 0;
+    }else if(perfil == 'pux1m'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaP'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaG'){
+        total = metro * 0;
+    }else if(perfil == 'puxBox'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucP'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucM'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucG'){
+        total = metro * 0;
+    }
+    return total;
+}
+
+function perfil2(){
+    let perfil = document.getElementById('tipoPerfil2').value;
+    let aux1 = document.getElementById('metro2');
+    let metro = Number(aux1.value);
+    let total = 0;
+
+    if(perfil == 'tuboPan'){
+        total = metro * 42;
+    }else if(perfil == 'tubo1x1'){
+        total = metro * 9;
+    }else if(perfil == 'tubo15x15'){
+        total = metro * 13;
+    }else if(perfil == 'tubo2x15'){
+        total = metro * 22;
+    }else if(perfil == 'tubo2x2'){
+        total = metro * 19;
+    }else if(perfil == 'tubo3x15'){
+        total = metro * 27;
+    }else if(perfil == 'tubo4x2'){
+        total = metro * 34;
+    }else if(perfil == 'tubo4x4'){
+        total = metro * 77;
+    }else if(perfil == 'tuboRed2'){
+        total = metro * 17;
+    }else if(perfil == 'tuboRed3'){
+        total = metro * 27;
+    }else if(perfil == 'reg5x5'){
+        total = metro * 5;
+    }else if(perfil == 'reg1x5'){
+        total = metro * 8;
+    }else if(perfil == 'reg2x5'){
+        total = metro * 15;
+    }else if(perfil == 'corre28'){
+        total = metro * 22;
+    }else if(perfil == 'cant12'){
+        total = metro * 5;
+    }else if(perfil == 'cant58'){
+        total = metro * 6;
+    }else if(perfil == 'cant34'){
+        total = metro * 7;
+    }else if(perfil == 'cant1'){
+        total = metro * 8;
+    }else if(perfil == 'jotinha'){
+        total = metro * 8;
+    }else if(perfil == 'jotao'){
+        total = metro * 14.5;
+    }else if(perfil == 'caval'){
+        total = metro * 10;
+    }else if(perfil == 'superCaval'){
+        total = metro * 16;
+    }else if(perfil == 'cantBag'){
+        total = metro * 14;
+    }else if(perfil == 'dobBox'){
+        total = metro * 21;
+    }else if(perfil == 'dobPort'){
+        total = metro * 8;
+    }else if(perfil == 'u8'){
+        total = metro * 5;
+    }else if(perfil == 'u10'){
+        total = metro * 6;
+    }else if(perfil == 'uGuard'){
+        total = metro * 11;
+    }else if(perfil == 'parTorreInox'){
+        total = metro * 0.4;
+    }else if(perfil == 'parInoxTorreInox'){
+        total = metro * 2;
+    }else if(perfil == 'torreInox'){
+        total = metro * 70;
+    }else if(perfil == 'pont15x15'){
+        total = metro * 25;
+    }else if(perfil == 'pont2x15'){
+        total = metro * 30;
+    }else if(perfil == 'ibere'){
+        total = metro * 15;
+    }else if(perfil == 'guarn39'){
+        total = metro * 1.7;
+    }else if(perfil == 'guarn44'){
+        total = metro * 0;
+    }else if(perfil == 'guarn51'){
+        total = metro * 2.4;
+    }else if(perfil == 'puxHquad'){
+        total = metro * 0;
+    }else if(perfil == 'puxHquadCrom'){
+        total = metro * 0;
+    }else if(perfil == 'puxHred'){
+        total = metro * 0;
+    }else if(perfil == 'puxHredCrom'){
+        total = metro * 0;
+    }else if(perfil == 'pux1m'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaP'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaG'){
+        total = metro * 0;
+    }else if(perfil == 'puxBox'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucP'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucM'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucG'){
+        total = metro * 0;
+    }
+    return total;
+}
+
+function perfil3(){
+    let perfil = document.getElementById('tipoPerfil3').value;
+    let aux1 = document.getElementById('metro3');
+    let metro = Number(aux1.value);
+    let total = 0;
+
+    if(perfil == 'tuboPan'){
+        total = metro * 42;
+    }else if(perfil == 'tubo1x1'){
+        total = metro * 9;
+    }else if(perfil == 'tubo15x15'){
+        total = metro * 13;
+    }else if(perfil == 'tubo2x15'){
+        total = metro * 22;
+    }else if(perfil == 'tubo2x2'){
+        total = metro * 19;
+    }else if(perfil == 'tubo3x15'){
+        total = metro * 27;
+    }else if(perfil == 'tubo4x2'){
+        total = metro * 34;
+    }else if(perfil == 'tubo4x4'){
+        total = metro * 77;
+    }else if(perfil == 'tuboRed2'){
+        total = metro * 17;
+    }else if(perfil == 'tuboRed3'){
+        total = metro * 27;
+    }else if(perfil == 'reg5x5'){
+        total = metro * 5;
+    }else if(perfil == 'reg1x5'){
+        total = metro * 8;
+    }else if(perfil == 'reg2x5'){
+        total = metro * 15;
+    }else if(perfil == 'corre28'){
+        total = metro * 22;
+    }else if(perfil == 'cant12'){
+        total = metro * 5;
+    }else if(perfil == 'cant58'){
+        total = metro * 6;
+    }else if(perfil == 'cant34'){
+        total = metro * 7;
+    }else if(perfil == 'cant1'){
+        total = metro * 8;
+    }else if(perfil == 'jotinha'){
+        total = metro * 8;
+    }else if(perfil == 'jotao'){
+        total = metro * 14.5;
+    }else if(perfil == 'caval'){
+        total = metro * 10;
+    }else if(perfil == 'superCaval'){
+        total = metro * 16;
+    }else if(perfil == 'cantBag'){
+        total = metro * 14;
+    }else if(perfil == 'dobBox'){
+        total = metro * 21;
+    }else if(perfil == 'dobPort'){
+        total = metro * 8;
+    }else if(perfil == 'u8'){
+        total = metro * 5;
+    }else if(perfil == 'u10'){
+        total = metro * 6;
+    }else if(perfil == 'uGuard'){
+        total = metro * 11;
+    }else if(perfil == 'parTorreInox'){
+        total = metro * 0.4;
+    }else if(perfil == 'parInoxTorreInox'){
+        total = metro * 2;
+    }else if(perfil == 'torreInox'){
+        total = metro * 70;
+    }else if(perfil == 'pont15x15'){
+        total = metro * 25;
+    }else if(perfil == 'pont2x15'){
+        total = metro * 30;
+    }else if(perfil == 'ibere'){
+        total = metro * 15;
+    }else if(perfil == 'guarn39'){
+        total = metro * 1.7;
+    }else if(perfil == 'guarn44'){
+        total = metro * 0;
+    }else if(perfil == 'guarn51'){
+        total = metro * 2.4;
+    }else if(perfil == 'puxHquad'){
+        total = metro * 0;
+    }else if(perfil == 'puxHquadCrom'){
+        total = metro * 0;
+    }else if(perfil == 'puxHred'){
+        total = metro * 0;
+    }else if(perfil == 'puxHredCrom'){
+        total = metro * 0;
+    }else if(perfil == 'pux1m'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaP'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaG'){
+        total = metro * 0;
+    }else if(perfil == 'puxBox'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucP'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucM'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucG'){
+        total = metro * 0;
+    }
+    return total;
+}
+
+function perfil4(){
+    let perfil = document.getElementById('tipoPerfil4').value;
+    let aux1 = document.getElementById('metro4');
+    let metro = Number(aux1.value);
+    let total = 0;
+
+    if(perfil == 'tuboPan'){
+        total = metro * 42;
+    }else if(perfil == 'tubo1x1'){
+        total = metro * 9;
+    }else if(perfil == 'tubo15x15'){
+        total = metro * 13;
+    }else if(perfil == 'tubo2x15'){
+        total = metro * 22;
+    }else if(perfil == 'tubo2x2'){
+        total = metro * 19;
+    }else if(perfil == 'tubo3x15'){
+        total = metro * 27;
+    }else if(perfil == 'tubo4x2'){
+        total = metro * 34;
+    }else if(perfil == 'tubo4x4'){
+        total = metro * 77;
+    }else if(perfil == 'tuboRed2'){
+        total = metro * 17;
+    }else if(perfil == 'tuboRed3'){
+        total = metro * 27;
+    }else if(perfil == 'reg5x5'){
+        total = metro * 5;
+    }else if(perfil == 'reg1x5'){
+        total = metro * 8;
+    }else if(perfil == 'reg2x5'){
+        total = metro * 15;
+    }else if(perfil == 'corre28'){
+        total = metro * 22;
+    }else if(perfil == 'cant12'){
+        total = metro * 5;
+    }else if(perfil == 'cant58'){
+        total = metro * 6;
+    }else if(perfil == 'cant34'){
+        total = metro * 7;
+    }else if(perfil == 'cant1'){
+        total = metro * 8;
+    }else if(perfil == 'jotinha'){
+        total = metro * 8;
+    }else if(perfil == 'jotao'){
+        total = metro * 14.5;
+    }else if(perfil == 'caval'){
+        total = metro * 10;
+    }else if(perfil == 'superCaval'){
+        total = metro * 16;
+    }else if(perfil == 'cantBag'){
+        total = metro * 14;
+    }else if(perfil == 'dobBox'){
+        total = metro * 21;
+    }else if(perfil == 'dobPort'){
+        total = metro * 8;
+    }else if(perfil == 'u8'){
+        total = metro * 5;
+    }else if(perfil == 'u10'){
+        total = metro * 6;
+    }else if(perfil == 'uGuard'){
+        total = metro * 11;
+    }else if(perfil == 'parTorreInox'){
+        total = metro * 0.4;
+    }else if(perfil == 'parInoxTorreInox'){
+        total = metro * 2;
+    }else if(perfil == 'torreInox'){
+        total = metro * 70;
+    }else if(perfil == 'pont15x15'){
+        total = metro * 25;
+    }else if(perfil == 'pont2x15'){
+        total = metro * 30;
+    }else if(perfil == 'ibere'){
+        total = metro * 15;
+    }else if(perfil == 'guarn39'){
+        total = metro * 1.7;
+    }else if(perfil == 'guarn44'){
+        total = metro * 0;
+    }else if(perfil == 'guarn51'){
+        total = metro * 2.4;
+    }else if(perfil == 'puxHquad'){
+        total = metro * 0;
+    }else if(perfil == 'puxHquadCrom'){
+        total = metro * 0;
+    }else if(perfil == 'puxHred'){
+        total = metro * 0;
+    }else if(perfil == 'puxHredCrom'){
+        total = metro * 0;
+    }else if(perfil == 'pux1m'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaP'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaG'){
+        total = metro * 0;
+    }else if(perfil == 'puxBox'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucP'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucM'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucG'){
+        total = metro * 0;
+    }
+
+    return total;
+}
+
+function perfil5(){
+    let perfil = document.getElementById('tipoPerfil5').value;
+    let aux1 = document.getElementById('metro5');
+    let metro = Number(aux1.value);
+    let total = 0;
+
+    if(perfil == 'tuboPan'){
+        total = metro * 42;
+    }else if(perfil == 'tubo1x1'){
+        total = metro * 9;
+    }else if(perfil == 'tubo15x15'){
+        total = metro * 13;
+    }else if(perfil == 'tubo2x15'){
+        total = metro * 22;
+    }else if(perfil == 'tubo2x2'){
+        total = metro * 19;
+    }else if(perfil == 'tubo3x15'){
+        total = metro * 27;
+    }else if(perfil == 'tubo4x2'){
+        total = metro * 34;
+    }else if(perfil == 'tubo4x4'){
+        total = metro * 77;
+    }else if(perfil == 'tuboRed2'){
+        total = metro * 17;
+    }else if(perfil == 'tuboRed3'){
+        total = metro * 27;
+    }else if(perfil == 'reg5x5'){
+        total = metro * 5;
+    }else if(perfil == 'reg1x5'){
+        total = metro * 8;
+    }else if(perfil == 'reg2x5'){
+        total = metro * 15;
+    }else if(perfil == 'corre28'){
+        total = metro * 22;
+    }else if(perfil == 'cant12'){
+        total = metro * 5;
+    }else if(perfil == 'cant58'){
+        total = metro * 6;
+    }else if(perfil == 'cant34'){
+        total = metro * 7;
+    }else if(perfil == 'cant1'){
+        total = metro * 8;
+    }else if(perfil == 'jotinha'){
+        total = metro * 8;
+    }else if(perfil == 'jotao'){
+        total = metro * 14.5;
+    }else if(perfil == 'caval'){
+        total = metro * 10;
+    }else if(perfil == 'superCaval'){
+        total = metro * 16;
+    }else if(perfil == 'cantBag'){
+        total = metro * 14;
+    }else if(perfil == 'dobBox'){
+        total = metro * 21;
+    }else if(perfil == 'dobPort'){
+        total = metro * 8;
+    }else if(perfil == 'u8'){
+        total = metro * 5;
+    }else if(perfil == 'u10'){
+        total = metro * 6;
+    }else if(perfil == 'uGuard'){
+        total = metro * 11;
+    }else if(perfil == 'parTorreInox'){
+        total = metro * 0.4;
+    }else if(perfil == 'parInoxTorreInox'){
+        total = metro * 2;
+    }else if(perfil == 'torreInox'){
+        total = metro * 70;
+    }else if(perfil == 'pont15x15'){
+        total = metro * 25;
+    }else if(perfil == 'pont2x15'){
+        total = metro * 30;
+    }else if(perfil == 'ibere'){
+        total = metro * 15;
+    }else if(perfil == 'guarn39'){
+        total = metro * 1.7;
+    }else if(perfil == 'guarn44'){
+        total = metro * 0;
+    }else if(perfil == 'guarn51'){
+        total = metro * 2.4;
+    }else if(perfil == 'puxHquad'){
+        total = metro * 0;
+    }else if(perfil == 'puxHquadCrom'){
+        total = metro * 0;
+    }else if(perfil == 'puxHred'){
+        total = metro * 0;
+    }else if(perfil == 'puxHredCrom'){
+        total = metro * 0;
+    }else if(perfil == 'pux1m'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaP'){
+        total = metro * 0;
+    }else if(perfil == 'puxBolaG'){
+        total = metro * 0;
+    }else if(perfil == 'puxBox'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucP'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucM'){
+        total = metro * 0;
+    }else if(perfil == 'bicoTucG'){
+        total = metro * 0;
+    }
+
+    return total;
+}
+
+function somaFinal() {
+    let precoFinal = perfil1() + perfil2() + perfil3() + perfil4() + perfil5()
+    let teste = document.getElementById('teste')
+    teste.innerHTML = `<br>O preço de custo foi: R$${precoFinal.toFixed(2)}\n`.replace(".", ",")
+    console.log('FIM-------------------------') 
+}
